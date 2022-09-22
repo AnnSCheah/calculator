2b6f4d1266202a7dd083574df0344ddd3b9134d9
Ann Shen Cheah <cheah.annshen@gmail.com>
2022-09-22 18:04:44 +0800

    Added basic javascript functionality logic for operators

--- javascript.js
+++ javascript.js
@@ -4,25 +4,76 @@ let operator,
   firstNumber = 0,
   secondNumber = 0;
 
-display.textContent = firstNumber;
+display.textContent = firstNumber; // * Display defaults to first number
 
+// * Operand and functionality buttons (clear, decimal, sign)
 const numbers = document.querySelectorAll(".operand");
 numbers.forEach((number) => {
   number.addEventListener("click", input);
 });
 
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", addDecimal);
+
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", clear);
 
 const signBtn = document.querySelector(".sign");
-signBtn.addEventListener("click", positiveNegative);
+signBtn.addEventListener("click", changeSign);
 
+// * Operator Event Listeners
+const addBtn = document.querySelector("button[value='plus']");
+addBtn.addEventListener("click", () => {
+  operator = add;
+  firstNumber = display.textContent;
+  display.textContent = secondNumber;
+});
+
+const subtractBtn = document.querySelector("button[value='minus']");
+subtractBtn.addEventListener("click", () => {
+  operator = subtract;
+  firstNumber = display.textContent;
+  display.textContent = secondNumber;
+});
+
+const multiplyBtn = document.querySelector("button[value='multiply']");
+multiplyBtn.addEventListener("click", () => {
+  operator = multiply;
+  firstNumber = display.textContent;
+  display.textContent = secondNumber;
+});
+
+const divideBtn = document.querySelector("button[value='divide']");
+divideBtn.addEventListener("click", () => {
+  operator = divide;
+  firstNumber = display.textContent;
+  display.textContent = secondNumber;
+});
+
+const equalsBtn = document.querySelector("button[value='equal']");
+equalsBtn.addEventListener("click", () => {
+  secondNumber = display.textContent;
+  display.textContent = operate(operator, firstNumber, secondNumber);
+  firstNumber = display.textContent;
+  secondNumber = 0;
+});
+
+// * Function that shows the operand in the display
 function input() {
   if (display.textContent.length >= 9) return; // * Limits the display to 9 digits
-  if (display.textContent != 0) {
-    display.textContent = parseInt(display.textContent) * 10;
+
+  if (display.textContent != 0 || display.textContent.includes(".")) {
+    display.textContent = display.textContent + this.value; // * Append the next input to the back instead of overwriting the current one
+  } else {
+    display.textContent = this.value;
   }
-  display.textContent = parseInt(display.textContent) + parseInt(this.value);
+}
+
+function addDecimal() {
+  if (display.textContent.includes(".")) {
+    return;
+  }
+  display.textContent = display.textContent + ".";
 }
 
 function clear() {
@@ -33,7 +84,7 @@ function clear() {
   display.textContent = firstNumber;
 }
 
-function positiveNegative() {
+function changeSign() {
   if (display.textContent == "0") return;
   display.textContent = parseInt(display.textContent) * -1;
 }
@@ -55,5 +106,5 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  return operator(a, b);
+  return operator(parseFloat(a), parseFloat(b));
 }
